7be1df29f6b24d777d8c4f2598647239542985bb
Maher ali <maher.techmasters@gmail.com>
2021-03-04 22:49:03 -0500

    fix fabio link

--- info.sh
+++ info.sh
@@ -1,4 +1,4 @@
-CLIENT=`multipass list | awk -F ' ' '(NR>1) $1 ~ "S" {printf "%s",$3; exit}' | awk '{print substr($0, 1, length($0))}'`
+CLIENT=`multipass list | awk -F ' ' '(NR>1) $1 ~ "C" {printf "%s",$3; exit}' | awk '{print substr($0, 1, length($0))}'`
 echo $CLIENT
 echo "Consul: http://${CLIENT}:8500/ui/"
 echo "Nomad:  http://${CLIENT}:4646/ui/"
